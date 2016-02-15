29042fa14ca6020e5580ba0614dacabe65dfe254
Adam Nekimken <adam.nekimken@gmail.com>
2016-02-15 13:56:32 -0800

    Final Paper edits

--- HairMechanicsPlots.m
+++ HairMechanicsPlots.m
@@ -4,8 +4,10 @@
 
 close all
 clear all
-save='No'
+save='Yes' %#ok<NOPTS>
 
+plotSize=[0.35 0.25 2.85 3.5/1.6-.5];
+plotPos=[0 2 3.5 3.5/1.6];
 
 % load one file for example plots
 FileName='ALN_Bend1.txt';
@@ -23,42 +25,59 @@ deflection=data{2}./1e6-data{3}.*sensitivity; % in m  % note: if deflection of c
 smoothedDeflection=smooth(deflection,21);
 
 % example of deformation profile
-figure(1)
-plot(0:0.001:size(smoothedDeflection,1)/1000-.001, smoothedDeflection*1e6,'k','LineWidth',2)
-% [hl1,ax2,ax3] = floatAxisX(0:0.001:size(smoothedDeflection,1)/1000-.001, smoothedDeflection*1e6,'k-','Time (s)');
-% [hl2,ax4,ax5] = floatAxisY(0:0.001:size(smoothedDeflection,1)/1000-.001, smoothedDeflection*1e6,'k-','Deflection (um)');
-% [hl2,ax2,ax3] = floatAxisX(psal,pres,'r:','Salinity (dotted)',[32 34 0 100]);
+figure('Units','inches',...
+'Position',plotPos,...
+'PaperPositionMode','auto',...
+'PaperSize',[3.5 3.5/1.6])
 
-xlabel('Time (s)','FontSize',24)
-ylabel('Deflection (um)','FontSize',24)
+
+plot(0:0.001:size(smoothedDeflection,1)/1000-.001, smoothedDeflection*1e6,'k','LineWidth',1)
+
+% xlabel('Time (s)','FontSize',12)
+% ylabel('Deflection ({\mu}m)','FontSize',12,'Interpreter','latex')
 ylimits=ylim;
 % xlimits=xlim;
 ylim([0 ylimits(2)])
-set(gca,'FontSize',24)
-set(gca,'Box','off')
-MiriamAxes(gca,'xy')
+set(gca,'FontSize',12)
+set(gca,'Box','off','Units','inches',...
+'ActivePositionProperty','Position',...
+'Position',plotSize)
+[newX,newY]=MiriamAxes(gca,'xy');
+set(get(newX,'XLabel'),'Visible','off')
+set(get(newY,'YLabel'),'Visible','off')
+% print(gcf,'-depsc','test.eps')
 
 
 
 %example of Force v deflection plot with best fit line
-figure(2)
-plot(deflection*1e6,CantileverForce*1e6,'k','DisplayName','Indentation Data')
+figure('Units','inches',...
+'ActivePositionProperty','Position',...
+'Position',plotPos,...
+'PaperPositionMode','auto',...
+'PaperSize',[3.5 3.5/1.6])
+plot(deflection*1e6,CantileverForce*1e6,'k','DisplayName','Indentation Data','LineWidth',0.5)
 ylimits=ylim;
-xlimits=xlim;
+xlimits=[0 60];
 hold all
 % title('Force vs. deflection','FontSize',24)
-set(gca,'FontSize',24)
+set(gca,'FontSize',12)
 
 Fdfit=fit(deflection*1e6,CantileverForce*1e6,'poly1');
 fitPlot=plot(Fdfit);
-set(fitPlot,'LineWidth',2)
-xlabel('Hair Deflection (um)')
-ylabel('Applied Force (uN)')
+set(fitPlot,'LineWidth',1)
+% xlabel('Hair Deflection ($\mu$m)','Interpreter','latex','FontName','Helvetica')
+% ylabel('Applied Force ($\mu$N)','Interpreter','latex')
 ylim(ylimits)
 xlim([0 xlimits(2)])
 legend off
-set(gca,'Box','off')
-MiriamAxes(gca,'xy')
+set(gca,'Box','off','Units','inches',...
+'ActivePositionProperty','Position',...
+'Position',plotSize)
+[newX,newY]=MiriamAxes(gca,'xy');
+set(get(newX,'XLabel'),'Visible','off')
+% set(newX,'Position',[0.18    0.11    0.775    0.0000000001]) % need to set same axes positions for all axes on all figures
+set(get(newY,'YLabel'),'Visible','off')
+% print(gcf,'-depsc','test2.eps')
 
 %% Stiffness vs. free length plot
 
@@ -69,42 +88,65 @@ FreeLength=zeros(size(daterTots));
 kPoints=cell(size(daterTots));
 kAvg=zeros(size(daterTots));
 errorBarSize=zeros(size(daterTots));
+fitData=zeros(size(daterTots));
+index=1;
+
 
 for i=1:size(daterTots,1)
     load(daterTots(i).name);
     for j=1:size(HairLengths,1)
         if strcmp(daterTots(i).name,HairLengths{j,1})
-            FreeLength(i)=HairLengths{j,2}; 
+            FreeLength(i)=HairLengths{j,2};
         end
     end
     kPoints{i}=stiffness;
     kAvg(i)=mean(kPoints{i});
     errorBarSize(i)=std(kPoints{i})/sqrt(size(kPoints{i},1));
+    for k=1:size(kPoints{i},1)
+        fitData(index,1)=FreeLength(i);
+        fitData(index,2)=kPoints{i}(k);
+        index=index+1;
+    end
 end
+inverseCubic=fittype('a/(x)^3');
+options=fitoptions('Method','NonLinearLeastSquares');
+Lkfit=fit(fitData(:,1),fitData(:,2),inverseCubic,options);
 
 %make plot
-figure(3)
+figure('Units','inches',...
+'Position',plotPos,...
+'PaperPositionMode','auto',...
+'PaperSize',[3.5 3.5/1.6])
 % errorbar(FreeLength,kAvg,errorBarSize,'kx')
 hold on
 for i=1:size(daterTots,1)
-    plot(FreeLength(i,1), kPoints{i,1},'ko','MarkerSize',6,'LineWidth',.75)
+    plot(FreeLength(i,1), kPoints{i,1},'ko','MarkerSize',5,'LineWidth',.75)
 end
 
-ylimits=ylim;
-ylim([0 ylimits(2)])
-set(gca,'FontSize',24)
-xlabel('Free Length of Hair (mm)')
-ylabel('Stiffness of Hair (N/m)')
-set(gca,'Box','off')
-MiriamAxes(gca,'x')
+fitPlot=plot(Lkfit);
+legend off
+set(fitPlot,'LineWidth',2)
 
+ylimits=ylim;
+ylim('auto')%[0 ylimits(2)])
+xlimits=xlim;
+xlim([4 xlimits(2)])
+set(gca,'FontSize',12)
+% xlabel('Free Length of Hair (mm)')
+% ylabel('Stiffness of Hair (N/m)')
+set(gca,'Box','off','Units','inches',...
+'ActivePositionProperty','Position',...
+'Position',plotSize)
+[newX,newY]=MiriamAxes(gca,'xy');
+set(get(newX,'XLabel'),'Visible','off')
+set(get(newY,'YLabel'),'Visible','off')
 
 %% Save everything
 if strcmp(save,'Yes')
-    saveas(figure(1),'/Users/adam/Documents/MATLAB/HairSwipe/Paper/DeflectionTimePlot.pdf')
+    saveas(figure(1),'/Users/adam/Documents/MATLAB/HairSwipe/Paper/DeflectionTimePlot','pdf')
     saveas(figure(1),'/Users/adam/Documents/MATLAB/HairSwipe/Paper/DeflectionTimePlot.fig')
-    saveas(figure(2),'/Users/adam/Documents/MATLAB/HairSwipe/Paper/ForceDistancePlot.pdf')
+    saveas(figure(2),'/Users/adam/Documents/MATLAB/HairSwipe/Paper/ForceDistancePlot','pdf')
     saveas(figure(2),'/Users/adam/Documents/MATLAB/HairSwipe/Paper/ForceDistancePlot.fig')
-    saveas(figure(3),'/Users/adam/Documents/MATLAB/HairSwipe/Paper/StiffnessVLengthPlot.pdf')
+    saveas(figure(3),'/Users/adam/Documents/MATLAB/HairSwipe/Paper/StiffnessVLengthPlot','pdf')
     saveas(figure(3),'/Users/adam/Documents/MATLAB/HairSwipe/Paper/StiffnessVLengthPlot.fig')
 end
